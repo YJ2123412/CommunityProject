7a500ba1b339a018c45014eabaf550a79a51101b
YJ2123412 <dpwls96923@gmail.com>
2024-03-07 11:26:38 +0900

    test

--- src/main/java/org/iclass/controller/RequestControllerMapping.java
+++ src/main/java/org/iclass/controller/RequestControllerMapping.java
@@ -3,13 +3,15 @@ package org.iclass.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.iclass.controller.community.ListController;
+
 
 public class RequestControllerMapping {
 	private static final Map<RequestKeyValue,Controller> mapping = new HashMap<>();
 	
 	public static void init() {
 		//설계된 url,method 에 따라 처리할 controller 구현체를 Key,Value 로 HashMap에 저장합니다.
-//		mapping.put(new RequestKeyValue("/community/list", "GET"), new ListController() );
+		mapping.put(new RequestKeyValue("/community/list", "GET"), new ListController() );
 //		mapping.put(new RequestKeyValue("/community/write","GET"), new WriteViewController() );
 //		mapping.put(new RequestKeyValue("/community/write","POST"), new WriteController() );
 //		//게시판 글 상세보기 ,수정하기,삭제하기
